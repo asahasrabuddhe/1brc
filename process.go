79bc5df81ebe1a1d1be575c82136a08af52ff8f2
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2024-12-02 15:19:50 +0530

    improve variable names

--- process.go
+++ process.go
@@ -14,7 +14,7 @@ import (
 
 const BufferSize = 32 * 1024 * 1024
 
-type Data struct {
+type Station struct {
 	Name  []byte
 	Min   int64
 	Max   int64
@@ -23,43 +23,26 @@ type Data struct {
 }
 
 var (
-	results = swiss.NewMap[uint64, Data](0)
+	results = swiss.NewMap[uint64, Station](0)
 	workers = runtime.NumCPU()
 )
 
 // Process reads the input data and returns the result
 func Process(r io.Reader, w io.Writer) error {
 	buf, leftover := make([]byte, BufferSize), []byte{}
-	linesCh, resultsCh, doneCh := make(chan []byte), make(chan *swiss.Map[uint64, Data], workers), make(chan struct{})
+	linesCh, resultsCh, aggregationDoneCh := make(chan []byte), make(chan *swiss.Map[uint64, Station], workers), make(chan struct{})
 
-	var wg sync.WaitGroup
+	var workerWaitGroup sync.WaitGroup
 
 	for i := 0; i < workers; i++ {
-		wg.Add(1)
+		workerWaitGroup.Add(1)
 		go func() {
 			processLines(linesCh, resultsCh)
-			wg.Done()
+			workerWaitGroup.Done()
 		}()
 	}
 
-	go func() {
-		for result := range resultsCh {
-			result.Iter(func(_ uint64, data Data) (stop bool) {
-				key := hash(data.Name)
-				if station, ok := results.Get(key); !ok {
-					results.Put(key, Data{data.Name, data.Min, data.Max, data.Total, 1})
-				} else {
-					station.Min = min(station.Min, data.Min)
-					station.Max = max(station.Max, data.Max)
-					station.Total += data.Total
-					station.Count += data.Count
-					results.Put(key, station)
-				}
-				return
-			})
-		}
-		doneCh <- struct{}{}
-	}()
+	go aggregateResults(resultsCh, aggregationDoneCh)
 
 	for {
 		n, err := r.Read(buf)
@@ -89,19 +72,19 @@ func Process(r io.Reader, w io.Writer) error {
 
 	close(linesCh)
 
-	wg.Wait()
+	workerWaitGroup.Wait()
 
 	close(resultsCh)
 
-	<-doneCh
+	<-aggregationDoneCh
 
 	printResult(w, results)
 
 	return nil
 }
 
-func processLines(linesChan chan []byte, resultsChan chan *swiss.Map[uint64, Data]) {
-	localResults := swiss.NewMap[uint64, Data](0)
+func processLines(linesChan chan []byte, resultsChan chan *swiss.Map[uint64, Station]) {
+	localResults := swiss.NewMap[uint64, Station](0)
 	for lines := range linesChan {
 		var lastIndex int
 		for index, c := range lines {
@@ -109,11 +92,11 @@ func processLines(linesChan chan []byte, resultsChan chan *swiss.Map[uint64, Dat
 				continue
 			}
 
-			name, temperature := processLine(lines[lastIndex:index])
-			key := hash(name)
+			stationName, temperature := processLine(lines[lastIndex:index])
+			key := hash(stationName)
 
 			if station, ok := localResults.Get(key); !ok {
-				localResults.Put(key, Data{name, temperature, temperature, temperature, 1})
+				localResults.Put(key, Station{stationName, temperature, temperature, temperature, 1})
 			} else {
 				station.Min = min(station.Min, temperature)
 				station.Max = max(station.Max, temperature)
@@ -134,12 +117,13 @@ func processLine(line []byte) ([]byte, int64) {
 	}
 
 	sepIndex := byteIndex(line, ';')
-	name := line[:sepIndex]
-	tempStr := line[sepIndex+1:]
+	name, tempStr := line[:sepIndex], line[sepIndex+1:]
 
-	var isNeg bool
+	var (
+		isNeg       bool
+		temperature int64
+	)
 	decimalIndex := byteIndex(tempStr, '.')
-	var temperature int64
 	for i, c := range tempStr {
 		if i == decimalIndex {
 			continue
@@ -157,9 +141,28 @@ func processLine(line []byte) ([]byte, int64) {
 	return name, temperature
 }
 
-func printResult(w io.Writer, data *swiss.Map[uint64, Data]) {
-	result := make([]Data, 0, data.Count())
-	data.Iter(func(_ uint64, v Data) (stop bool) {
+func aggregateResults(resultsCh chan *swiss.Map[uint64, Station], aggregationDoneCh chan struct{}) {
+	for result := range resultsCh {
+		result.Iter(func(_ uint64, data Station) (stop bool) {
+			key := hash(data.Name)
+			if station, ok := results.Get(key); !ok {
+				results.Put(key, Station{data.Name, data.Min, data.Max, data.Total, 1})
+			} else {
+				station.Min = min(station.Min, data.Min)
+				station.Max = max(station.Max, data.Max)
+				station.Total += data.Total
+				station.Count += data.Count
+				results.Put(key, station)
+			}
+			return
+		})
+	}
+	aggregationDoneCh <- struct{}{}
+}
+
+func printResult(w io.Writer, data *swiss.Map[uint64, Station]) {
+	result := make([]Station, 0, data.Count())
+	data.Iter(func(_ uint64, v Station) (stop bool) {
 		result = append(result, v)
 		return
 	})
@@ -178,8 +181,8 @@ func printResult(w io.Writer, data *swiss.Map[uint64, Data]) {
 	_, _ = fmt.Fprint(w, "}\n")
 }
 
-func byteIndex(s []byte, sep byte) int {
-	for i, c := range s {
+func byteIndex(src []byte, sep byte) int {
+	for i, c := range src {
 		if c == sep {
 			return i
 		}
@@ -187,11 +190,11 @@ func byteIndex(s []byte, sep byte) int {
 	return -1
 }
 
-func hash(name []byte) uint64 {
-	n := min(len(name), 10) // 10 bytes, one more than we need just to be safe
+func hash(in []byte) uint64 {
+	n := min(len(in), 10) // 10 bytes, one more than we need just to be safe
 	var result uint64
 
-	for _, b := range name[:n] {
+	for _, b := range in[:n] {
 		v := b - 65
 		m := uint64(10 << ((v / 10) * 3)) // Compute scaling factor efficiently
 		result = result*m | uint64(b)
